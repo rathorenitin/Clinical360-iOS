e8b5bbf67b7b74b5c09b28c132da2f9f739fc194
Nitin Singh Rathore <rathorenitinsr@gmail.com>
2026-06-13 20:20:11 +0530

    Initial commit
    - Project Setup

---
+++ Clinical360/Clinical360/Clinical360App.swift
@@ -0,0 +1,17 @@
+//
+//  Clinical360App.swift
+//  Clinical360
+//
+//  Created by Nitin Singh Rathore on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Clinical360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clinical360/Clinical360Tests/Clinical360Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Clinical360Tests.swift
+//  Clinical360Tests
+//
+//  Created by Nitin Singh Rathore on 13/06/26.
+//
+
+import Testing
+@testable import Clinical360
+
+struct Clinical360Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
